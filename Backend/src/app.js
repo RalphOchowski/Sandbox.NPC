3f7b7e524956923cb5da08b64620052da37aaaa0
RalphOchowski <ralphochowski@gmail.com>
2026-06-10 20:44:46 +0530

    First Attempt At Complete Deployment

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,16 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 
 const app = express();
+const _dirname = path.resolve();
 const port = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+//deployment prep
+if(process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(_dirname, "../frontend/dist/")));
+	app.get(/.*/, (req, res) => {
+		res.sendFile(path.join(_dirname, "../frontend/dist/index.html"))
+	});
+}
+	
 app.listen(port, () => {
     console.log(`I guess we're running at localhost:${port}...`);
 });
