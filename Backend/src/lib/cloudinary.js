467f267caf10427a50e9486b293cb3f60650e9ad
RalphOchowski <ralphochowski@gmail.com>
2026-06-17 21:28:29 +0530

    Added an auth-check endpoint to check if user is authenticated/signed in, upon hitting refresh.

--- Backend/src/lib/cloudinary.js
+++ Backend/src/lib/cloudinary.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";// ??? take note for sure
-import { CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY } from "./env.js";
+import { CLOUDINARY_API_SECRET, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY } from "./config.js";
 
 cloudinary.config({
   cloud_name: CLOUDINARY_CLOUD_NAME,
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -9,5 +9,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.put("/update-profile", protectRoute, updateProfile); //added the first middleware function in the middle that can now be used to authenticate the user in order to call the adjacent controller function. Broad definition for middleware being any function that is ran before sending a response back to the client
+router.get("/auth-check", protectRoute, (req, res) => res.status(200).json(req.user));//take note, used to check if the user is authenticated and logged in incase they hit refresh
 
 export default router;
